1d8aeab0d9eaf2b8e63224159c1c9307a345777d
Alex Younger <alexanderyounger@gmail.com>
2020-05-04 13:30:32 -0400

    Ready for soft deployment

---
+++ contactscript.js
@@ -0,0 +1,59 @@
+const submitButton = document.querySelector('.submit');
+const contactForm = document.querySelector('#contact-form');
+const invalidModal = document.querySelector('.invalid');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const topicInput = document.querySelector('#topic');
+const textField = document.querySelector('textarea');
+
+const validateName = () => {
+    if (/^[a-zA-Z]+ [a-zA-Z]+/g.test(nameInput.value)) {
+        return true;
+    } else {
+        invalidModal.textContent += 'Please enter a valid name (first and last).'
+        invalidModal.style.display = 'flex';
+        return false;
+    }
+}
+
+const validateEmail = () =>  {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailInput.value)) {
+        return true;
+    } else {
+        invalidModal.textContent += 'Please enter a valid email address.'
+        invalidModal.style.display = 'flex';
+        return false;
+    }
+}
+
+const validateTopic = () => {
+    if (topicInput.value !== 'null') {
+        return true;
+    } else {
+        invalidModal.textContent += 'Please select an option from the dropdown.'
+        invalidModal.style.display = 'flex';
+        return false;
+    }
+}
+
+const validateText = () => {
+    if (textField.value.length >= 10) {
+        return true;
+    } else {
+        invalidModal.textContent += 'Please type a message in the text field (minimum 10 characters).'
+        invalidModal.style.display = 'flex';
+        return false;
+    }
+}
+
+const formValidation = () => {
+    return (validateName() && validateEmail() && validateTopic() && validateText())
+}
+
+submitButton.addEventListener('click', () => {
+    invalidModal.textContent = '';
+    invalidModal.style.display = 'none';
+    if (formValidation()) {
+        document.querySelector('#contact-form').submit();
+    }
+})
---
+++ script.js
@@ -0,0 +1,48 @@
+const header = document.querySelector('header');
+const hamburger = document.querySelector('.menu');
+const nav = document.querySelector('nav');
+const navClone = nav.cloneNode(true);
+const navItems = navClone.querySelectorAll('.nav');
+const mobileNavMenu = document.createElement('div');
+const blurAndDim = document.createElement('div');
+blurAndDim.className = 'blurdim';
+document.querySelector('body').insertBefore(blurAndDim, header);
+mobileNavMenu.className = 'mobile-nav-menu';
+
+
+const mobileNavDeactive = () => {
+    mobileNavMenu.style.display = 'none';
+    blurAndDim.style.display = 'none';
+    hamburger.style.backgroundColor = 'inherit';
+}
+
+const mobileNavActive = () => {
+    navItems.forEach(item => {
+        mobileNavMenu.appendChild(item);
+        if (item !== navItems[navItems.length - 1]) {
+            mobileNavMenu.appendChild(document.createElement('hr'));
+        }
+    })
+    mobileNavMenu.style.display = 'flex';
+    blurAndDim.style.display = 'flex';
+    hamburger.style.backgroundColor = 'rgba(85, 106, 119, 0.3)';
+    blurAndDim.addEventListener('click', () => {
+        mobileNavMenu.style.display = 'none';
+        blurAndDim.style.display = 'none';
+        hamburger.style.backgroundColor = 'inherit';
+    })
+}
+
+header.appendChild(mobileNavMenu);
+
+hamburger.addEventListener('click', () => {
+    if (mobileNavMenu.style.display === 'flex') {
+        mobileNavDeactive();
+    } else {
+        mobileNavActive();
+    }
+    blurAndDim.addEventListener('click', () => {
+        mobileNavDeactive();
+        return;
+    })
+})
